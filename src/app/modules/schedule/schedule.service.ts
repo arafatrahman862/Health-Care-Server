88038e4f4d44c20fa97875e975a0aa44d8a384a1
Arafat Rahman <arafatrahman862@gmail.com>
2025-11-13 18:38:20 +0600

    schedule mvc added

---
+++ src/app/modules/schedule/schedule.service.ts
@@ -0,0 +1,8 @@
+const insertIntoDB = async (payload: any) => {
+    console.log({ payload });
+  return payload;
+};
+
+export const ScheduleService = {
+  insertIntoDB,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,19 +1,24 @@
 import express from 'express';
 import { userRoutes } from '../modules/user/user.routes';
 import { authRoutes } from '../modules/auth/auth.routes';
+import { scheduleRoutes } from '../modules/schedule/schedule.routes';
 
 
 const router = express.Router();
 
 const moduleRoutes = [
-    {
-        path: '/user',
-        route: userRoutes
-    },
-    {
-        path: '/auth',
-        route: authRoutes
-    },
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+  {
+    path: "/auth",
+    route: authRoutes,
+  },
+  {
+    path: "/schedule",
+    route: scheduleRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
