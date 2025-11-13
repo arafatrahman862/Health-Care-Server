4301ee82bd335493c5caa110724494d6c3925919
Arafat Rahman <arafatrahman862@gmail.com>
2025-11-13 20:39:50 +0600

    schedule date and time added

--- src/app/modules/schedule/schedule.service.ts
+++ src/app/modules/schedule/schedule.service.ts
@@ -1,5 +1,57 @@
+import { addHours, addMinutes, format } from "date-fns";
+import { prisma } from "../../shared/prisma";
+
 const insertIntoDB = async (payload: any) => {
-    console.log({ payload });
+    const {startTime, endTime, startDate, endDate} = payload;
+    const intervalTime = 30; // minutes
+
+    const schedules = [];
+
+    const currentDate = new Date(startDate);
+    const lastDate = new Date(endDate);
+
+while(currentDate <= lastDate){
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0])
+        ),
+        Number(startTime.split(":")[1])
+      )
+    );
+    const endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0])
+        ),
+        Number(endTime.split(":")[1])
+      )
+    );
+    while (startDateTime < endDateTime){
+        const slotStartDateTime = startDateTime;
+        const slotEndDateTime = addMinutes(startDateTime, intervalTime);
+
+        const scheduleData = {
+            startDateTime: slotStartDateTime,
+            endDateTime: slotEndDateTime
+        }
+
+        const existingSchedule = await prisma.schedule.findFirst({
+          where: scheduleData,
+        });
+
+        if(!existingSchedule){
+          const result = await prisma.schedule.create({
+            data: scheduleData
+          });
+          schedules.push(result)
+        }
+    }
+    
+}
+
   return payload;
 };
 
